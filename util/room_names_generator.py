70d964d553a492a2049873728d2b661bd84bc8c3
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-23 13:13:33 -0700

    code clean up for declarative var names

--- util/room_names_generator.py
+++ util/room_names_generator.py
@@ -2,23 +2,20 @@ from django.contrib.auth.models import User
 from adventure.models import Player, Room
 
 f = open('room_names.txt', 'r')
-room_names = f.read().split("\n")
+room_names_file = f.read().split("\n")
 f.close()
 
-room_names_map = {}
-room_array = []
+room_names_dict = {}
+room_objects_array = []
 
-for name in room_names:
-    room_names_map[f"r_{name.lower().replace(' ', '_')}"] = Room(title=name, description=name)
+for name in room_names_file:
+    room_names_dict[f"r_{name.lower().replace(' ', '_')}"] = Room(title=name, description=name)
 
-for room in room_names_map:
-    # room_names_map[room].save()
-    room_array.append(room_names_map[room])
+for room in room_names_dict:
+    room_objects_array.append(room_names_dict[room])
 
-for i in range(len(room_array)):
-    room_array[i].save()
-    # room_array[i].connectRooms(room_array[i + 1], "n")
+for i in range(len(room_objects_array)):
+    room_objects_array[i].save()
 
-for i in range(len(room_array) - 1):
-    # room_array[i].save()
-    room_array[i].connectRooms(room_array[i + 1], "n")
+for i in range(len(room_objects_array) - 1):
+    room_objects_array[i].connectRooms(room_objects_array[i + 1], "n")
