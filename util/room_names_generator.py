a1531bd8f27f1e92a177fd7506c50f7f235f6790
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-23 12:44:43 -0700

    saves rooms in db

--- util/room_names_generator.py
+++ util/room_names_generator.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from adventure.models import Player, Room
+
 f = open('room_names.txt', 'r')
 room_names = f.read().split("\n")
 f.close()
@@ -5,6 +8,7 @@ f.close()
 room_names_map = {}
 
 for name in room_names:
-    room_names_map[f"r_{name.lower().replace(' ', '_')}"] = name
+    room_names_map[f"r_{name.lower().replace(' ', '_')}"] = Room(title=name, description=name)
 
-print(room_names_map)
+for room in room_names_map:
+    room_names_map[room].save()
