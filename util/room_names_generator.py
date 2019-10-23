d5871ca2e2a1215fab85e469068909efe59a6ae8
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-23 12:57:48 -0700

    connect all room to n

--- util/room_names_generator.py
+++ util/room_names_generator.py
@@ -6,9 +6,19 @@ room_names = f.read().split("\n")
 f.close()
 
 room_names_map = {}
+room_array = []
 
 for name in room_names:
     room_names_map[f"r_{name.lower().replace(' ', '_')}"] = Room(title=name, description=name)
 
 for room in room_names_map:
-    room_names_map[room].save()
+    # room_names_map[room].save()
+    room_array.append(room_names_map[room])
+
+for i in range(len(room_array)):
+    room_array[i].save()
+    # room_array[i].connectRooms(room_array[i + 1], "n")
+
+for i in range(len(room_array) - 1):
+    # room_array[i].save()
+    room_array[i].connectRooms(room_array[i + 1], "n")
