7cf056440010ac85eac3062dcea2c57d43fec127
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-24 08:43:55 -0700

    Added api endpoint location, now i have to actually develop the get_map

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from django.conf.urls import url
+from api import api
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
+    path('map/', api.get_map),
 ]
